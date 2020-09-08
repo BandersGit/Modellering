2d142260b9502892ff4d957f36e25960a854f47a
BandersGit <andy.i.flaskan@gmail.com>
2020-09-08 10:31:46 +0200

    Initial commit

---
+++ Modellering/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Modellering
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+
+
+
+
+
+
+            
+        }
+    }
+}
